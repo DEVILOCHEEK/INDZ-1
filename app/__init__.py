8d9ae5fdc5a4c77bbb26cba0634a765804688f38
DEVILOCHEEK <123664311+DEVILOCHEEK@users.noreply.github.com>
2025-05-22 00:26:54 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
-from flask import Flask
-from .routes import register_routes
-from .database import init_db
+from flask import Flask, jsonify
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:pass@localhost:5432/tododb'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    init_db(app)
-    register_routes(app)
+    @app.route('/healthcheck')
+    def healthcheck():
+        return jsonify({'status': 'ok'})
+
+    # Тут інші ініціалізації, наприклад, бази даних, маршрути
 
     return app
